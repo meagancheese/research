a0619e621e1a4900351b32b6d2c4f8aecedb0006
meagancheese <meagan.konst@wustl.edu>
2022-12-16 12:19:33 -0600

    Changed pointer math to array indexing

--- preprocess.cpp
+++ preprocess.cpp
@@ -40,7 +40,7 @@ int ped_subtract(struct SW_Data_Packet * data_packet, uint16_t *all_peds) {
     int ped_sample_idx = data_packet->starting_sample_number;
     for (int i = 0; i < data_packet->samples_to_be_read + 1; i++) {
         for (int j = 0; j < NUM_CHANNELS; j++) {
-            ped_sub_results[i][j] = data_packet->samples[i][j] - *(((all_peds + (data_packet->bank)*(NUM_SAMPLES*NUM_CHANNELS)) + ped_sample_idx*NUM_CHANNELS) + j);
+            ped_sub_results[i][j] = data_packet->samples[i][j] - all_peds[(data_packet->bank)*NUM_SAMPLES*NUM_CHANNELS + ped_sample_idx*NUM_CHANNELS + j];
         }
         ped_sample_idx += 1;
         if (ped_sample_idx == NUM_SAMPLES) {
